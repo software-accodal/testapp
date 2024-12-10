a81b5c34aa3a25ce4b6abff707a8c13601273d8f
software-accodal <software@accodal.com>
2024-12-11 00:20:58 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,9 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [missive, setMissive] = useState();
-  const [conversationIds, setConversationIds] = useState([]);
+  useEffect(() => console.log("test"));
 
-  useEffect(() => {
-    if (!missive) {
-      console.log("initialRun");
-      setMissive(window.Missive);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!missive) return;
-
-    console.log("missive");
-    missive.on("change:conversations", (ids) => setConversationIds(ids || []), {
-      retroactive: true,
-    });
-  }, [missive]);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <h1>TEST</h1>;
 }
 
 export default App;
